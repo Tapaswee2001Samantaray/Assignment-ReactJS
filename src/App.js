325da9905ac77276a8cfd130499e5bfea0d54a10
Tapaswee2001Samantaray <tapas10231@gmail.com>
2023-06-28 11:13:35 +0530

    assignment done

--- src/App.js
+++ src/App.js
@@ -1,51 +1,17 @@
-import { useState } from "react";
-import About from "./components/About";
-import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
-import Alert from "./components/Alert";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const users = [
+  { name: "John Doe", id: 1 },
+  { name: "Jane Doe", id: 2 },
+  { name: "Billy Doe", id: 3 }
+];
 
 function App() {
-  const [mode, setMode] = useState("light");
-  const [alert, setAlert] = useState(null);
-
-  function showAlert(message, type) {
-    setAlert({
-      msg: message,
-      type: type
-    });
-
-    setTimeout(() => {
-      setAlert(null);
-    }, 3000);
-  }
-
-  function toggleMode() {
-    if (mode === "light") {
-      setMode("dark");
-      document.body.style.backgroundColor = "#0b203e";
-      showAlert("Dark mode has been enabled.", "success");
-      document.title = "TextUtils - Dark Mode";
-    } else {
-      setMode("light");
-      document.body.style.backgroundColor = "white";
-      showAlert("Light mode has been enabled.", "success");
-      document.title = "TextUtils - Light Mode";
-    }
-  }
-
+  // const userItems = users.map((user) => <li key={user.id}>{user.name}</li>);
   return (
     <>
-      <Router>
-        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} aboutText="About" />
-        <Alert alert={alert} />
-        <div className="container my-3">
-          <Routes>
-            <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />} />
-            <Route exact path="/about" element={<About />} />
-          </Routes>
-        </div>
-      </Router>
+      <h2>User Datails</h2>
+      {/* <ul>{userItems}</ul> */}
+      <ul>{users.map((user) => <li key={user.id}>{user.name}</li>)}</ul>
     </>
   );
 }
